d5091324333703ff4d3fa19bd68cfacf8ceeeb78
gao peng <gpqhl0071@126.com>
2016-03-07 15:56:27 +0800

    问题1

---
+++ src/com/kata/demo/Sequence.java
@@ -0,0 +1,16 @@
+/*
+*Copyright (c) 2016, gp.inc and/or its affiliates. All rights reserved.
+*/
+package com.kata.demo;
+
+public class Sequence {
+	public static final long BASE = 50;
+	
+	public static long getScore(long n) {
+		long sum = 0;
+		for (long i = n; i > 0; i--) {
+			sum += i;
+		}
+		return sum * BASE;
+	}
+}
