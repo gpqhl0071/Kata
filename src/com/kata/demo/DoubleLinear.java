a013a6577d04695b0bbb26957a5a4fdf257358c6
gao peng <gpqhl0071@126.com>
2016-05-16 11:38:38 +0800

    Twice linear

--- src/com/kata/demo/DoubleLinear.java
+++ src/com/kata/demo/DoubleLinear.java
@@ -1,6 +1,3 @@
-/*
-*Copyright (c) 2016, gp.inc and/or its affiliates. All rights reserved.
-*/
 package com.kata.demo;
 
 import java.util.HashMap;
@@ -8,12 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 
- * @author: gao peng
- * @date: 2016年5月13日 下午5:20:20
- *
- */
 public class DoubleLinear {
 
 	static List<Integer> list;
@@ -26,10 +17,11 @@ public class DoubleLinear {
 
 		map.put(base, base);
 		list.add(1);
-		while (list.size() < n) {
+		while (list.size() <= n * 2) {
 			execute();
 		}
 
+		sort(list);
 		return list.get(n);
 	}
 
@@ -40,29 +32,68 @@ public class DoubleLinear {
 			int a = 2 * value + 1;
 			// 3*n+1
 			int b = 3 * value + 1;
-			nextMap.put(a, a);
-			nextMap.put(b, b);
+			if (!nextMap.containsKey(a)) {
+				nextMap.put(a, a);
+			}
+			if (!nextMap.containsKey(b)) {
+				nextMap.put(b, b);
+			}
 		}
 
 		Map<Integer, Integer> cloneNextMap = new HashMap<Integer, Integer>();
 		cloneNextMap.putAll(nextMap);
-		for (Integer min : nextMap.values()) {
-			for (Integer value : cloneNextMap.values()) {
-				if (min > value) {
-					min = value;
-				}
-			}
-			cloneNextMap.remove(min);
-			list.add(min);
-		}
+
+		addList(nextMap);
 
 		map.clear();
 		map.putAll(nextMap);
 	}
 
 	public static void main(String[] args) {
-//		System.out.println(dblLinear(10));
+		System.out.println(dblLinear(10));
 		System.out.println(dblLinear(20));
 		System.out.println(dblLinear(30));
+		System.out.println(dblLinear(50));
+	}
+
+	public static void addList(Map<Integer, Integer> map) {
+		Integer[] values = new Integer[map.size()];
+		int num = 0;
+		for (Integer value : map.values()) {
+			values[num] = value;
+			num++;
+		}
+
+		for (int i = 0; i < values.length; i++) {
+			if (list.contains(values[i])) {
+				continue;
+			}
+			list.add(values[i]);
+		}
+
+	}
+
+	public static void sort(List<Integer> l) {
+		int[] values = new int[l.size()];
+		for (int i = 0; i < l.size(); i++) {
+			values[i] = l.get(i);
+		}
+
+		l.clear();
+		int temp = 0;
+		for (int i = 0; i < values.length - 1; i++) {
+			for (int j = i + 1; j < values.length; j++) {
+				if (values[i] > values[j]) {
+					temp = values[i];
+					values[i] = values[j];
+					values[j] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < values.length; i++) {
+			list.add(values[i]);
+		}
+		System.out.println(list.toString());
 	}
 }
